7bd51e2a9780b2b74047c86c2a4ea1d5a9c26c25
svladkov <s.v.vladkov@gmail.com>
2017-10-18 14:57:04 +0300

    Created view and template to roughly display data about all matches

--- bundesliga/urls.py
+++ bundesliga/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^bundesliga_app', include('bundesliga_app.urls')),
+    url(r'', include('bundesliga_app.urls'))
 ]
--- bundesliga_app/views.py
+++ bundesliga_app/views.py
@@ -1,3 +1,20 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+import json
+import urllib
 
-# Create your views here.
+def index(request):
+    return HttpResponse("Hello world")
+
+def all_matches(request, league_shortcut, league_season):
+    base_url = 'https://www.openligadb.de/api/getmatchdata/'
+    url = base_url + league_shortcut + "/" + league_season
+    response = urllib.request.urlopen(url).read()
+    all_matches_data_as_string = response.decode(encoding='UTF-8')
+    all_matches_data = json.loads(all_matches_data_as_string)
+
+    context = {
+        'all_matches_data': all_matches_data
+    }
+
+    return render(request, 'bundesliga_app/index.html', context)
